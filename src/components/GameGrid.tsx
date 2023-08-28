58828d61d4fca4c7ab72b5b66169fae766837496
marykasp <kasparian.mary@gmail.com>
2023-08-28 09:44:44 -0500

    style: add images to genre sidebar list along with title

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ const GameGrid = () => {
   return (
     <div>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 3, lg: 5 }} spacing={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={5}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
